03d8d14a4968d1ba86de3e5a9338799559f06098
Joseph Broome <josephlloydbroome@gmail.com>
2022-06-28 09:36:20 -0700

    updates

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@
         double weight = 75;
         decimal height = 6;
 
-        Console.WriteLine($"There once was a man named {personName}.{personName} was {personAge} and his nickname was {nickName}. It is {lovesToEat} that {personName} loves to eat and because of that he is {weight} lbs.{personName} is only {height} foot and is considered overweight");
+        Console.WriteLine($"There once was a man named {personName}.{personName} was {personAge} and his nickname was {nickName}. It is {lovesToEat} that {personName} loves to eat and because of that he is {weight} lbs.{personName} is only {height} foot and is considered overweight.");
     }
 
 }
