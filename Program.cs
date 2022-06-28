4911c7bdf85f9b4e5ca0acb6e6bda20d4019f82e
Joseph Broome <josephlloydbroome@gmail.com>
2022-06-28 09:35:36 -0700

    updates

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+
+    static void Main(string[] args)
+    {
+        string personName = "Brad";
+        int personAge = 25;
+
+        char nickName = 'B';
+        bool lovesToEat = true;
+
+        double weight = 75;
+        decimal height = 6;
+
+        Console.WriteLine($"There once was a man named {personName}.{personName} was {personAge} and his nickname was {nickName}. It is {lovesToEat} that {personName} loves to eat and because of that he is {weight} lbs.{personName} is only {height} foot and is considered overweight");
+    }
+
+}
